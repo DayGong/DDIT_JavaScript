187bac95e5e10b986cd578a01f8c668ff7d6dad4
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 03:36:13 +0900

    Create inTestMod.js

---
+++ Module/inTestMod.js
@@ -0,0 +1,15 @@
+const fck1 = function(){
+    alert("fck1");
+}
+
+const fck2 = function(){
+    alert("fck2");
+}
+
+const fck3 = function(){
+    alert("fck3");
+}
+
+const set = { fck1, fck2, fck3 };
+
+export default set;     // 수출
